481f2f081c995df96f18b30e992321bb0c000f45
edimdendidiwangga <edimdendy@gmail.com>
2017-04-12 16:54:01 +0700

    orm student schema edim

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 "use strict"
+let db = require('./models')
+let data = {
+	        firstname: 'John',
+	        lastname: 'Doe',
+	        birthdate: '1990-09-16',
+	        email: 'john@mail.org',
+	        height: 140,
+	        phone: '08912345678912',
+	        createdAt: new Date().toISOString(),
+	        updatedAt: new Date().toISOString()
+    	}
+function addData(data) {
+  db.Student.create({firstname: data.firstname, lastname: data.lastname, birthdate: data.birthdate, email: data.email, height: data.height, phone: data.phone})
+  .then(function(){
+    console.log(`${data.firstname} inserted`);
+  }).catch(function(err){
+    console.log(err.message);
+  })
+}
+
+function getAllStudentData() {
+  db.Student.getAllData((cb)=>{
+  	cb.forEach((res)=>{
+  	  console.log(res.id);
+  	  console.log(res.firstname);
+      console.log(res.lastname);
+      console.log(res.fullname);
+    })
+  })
+}
+
+function getAllAgeStudent() {
+  db.Student.getAllAge((cb)=>{
+  	cb.forEach((res)=>{
+      console.log(`${res.fullname} was age is ${res.age}`);
+    })
+  })
+    
+}
+
+//addData(data)
+getAllStudentData()
+getAllAgeStudent()
