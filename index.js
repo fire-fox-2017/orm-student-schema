6c3bf7d9fd69f6b496641487a0a57fe42c7542b3
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-04-12 12:34:49 +0700

    added instance method getFullName and getAge

--- index.js
+++ index.js
@@ -1,4 +1,43 @@
 "use strict"
+const db = require('./models');
+const repl = require('repl');
 
-const sqlite = require('sqlite3').verbose();
+let replServer = repl.start({prompt: '$_$> '});
+
+// Seeding
+// db.Student.create({first_name: "David", last_name: "Johnson", birthdate: "1990-01-02"});
+// db.Student.create({first_name: "Jamie", last_name: "Lamb", birthdate: "1991-01-02"});
+// db.Student.create({first_name: "Ben", last_name: "Roth", birthdate: "1988-03-14"});
+// db.Student.create({first_name: "Brandin", last_name: "Cook", birthdate: "1991-11-20"});
+
+
+
+let getFullName = (id) => {
+	db.Student.findById(id)
+		.then(student => {
+			// console.log(student.toJSON());
+			console.log(student.getFullName());
+		})
+		.catch(err => {
+			console.log(err.message);
+		})
+
+}
+
+let getAge = (id) => {
+	db.Student.findById(id)
+		.then(student => {
+			// console.log(student.toJSON());
+			console.log(student.getAge());
+		})
+		.catch(err => {
+			console.log(err.message);
+		})
+
+}
+
+replServer.context.getFullName = getFullName;
+replServer.context.getAge = getAge;
+
+
 
--- models/student.js
+++ models/student.js
@@ -3,13 +3,26 @@ module.exports = function(sequelize, DataTypes) {
   var Student = sequelize.define('Student', {
     first_name: DataTypes.STRING,
     last_name: DataTypes.STRING,
-    birtdate: DataTypes.DATE
+    birthdate: DataTypes.DATE
   }, {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
       }
-    }
+    },
+    instanceMethods: {
+      getFullName: function() {
+        return `${this.first_name} ${this.last_name}`;
+      },
+      getAge: function() {
+        let now = new Date(Date.now());
+        console.log(`birthdate: ${this.birthdate}`);
+        let age = now.getYear() - this.birthdate.getYear();
+        
+        return age;
+      }
+
+    }// end of instanceMethods
   });
   return Student;
 };
