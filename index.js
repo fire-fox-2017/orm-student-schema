0437d5fed1648794546c3d928526d970d47a81ff
aldyadk <aldy.andika@gmail.com>
2017-04-12 22:19:08 +0700

    komit utak atik ga kelar2

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
 "use strict"
+const repl = require('repl');
+const replServer = repl.start({prompt:'>=>=> '});
+const folder = require('./models');
+
+let insertStudent = (firstname,lastname,birthdate,email,height,phone) => {
+  folder.Student.create({
+    first_name : firstname,
+    "last_name" : lastname,
+    "birth_date" : birthdate,
+    "email" : email,
+    "height" : height,
+    "phone" : phone
+  })
+  .then(() => {
+    console.log('success!');
+  })
+  .catch((err) => {
+    //console.log(db.Student.msg);
+    console.log(err.message);
+  })
+}
+
+let showAllStudent = function (){
+  folder.Student.findAll().then(student =>{
+    for(let i=0;i<student.length;i++){
+      console.log(student[i].getFullName());
+    }
+  })
+}
+
+let showAllStudentAge = function (){
+  folder.Student.findAll().then(student =>{
+    for(let i=0;i<student.length;i++){
+      console.log(student[i].getAge());
+    }
+  })
+}
+
+let showAllStudentDetail = function(){
+  folder.Student.getAllData(function(students){
+    // students.forEach(function(student){
+    //   console.log(student.id);
+    //   console.log(student.first_name);
+    //   console.log(student.last_name);
+    //   console.log(student.full_name);
+    // })
+    for(let i=0;i<students.length;i++){
+      console.log(students[i].id);
+      console.log(students[i].first_name);
+      console.log(students[i].last_name);
+      console.log(students[i].full_name);
+    }
+  })
+}
+
+
+
+replServer.context.a = showAllStudent;
+replServer.context.b = showAllStudentAge;
+replServer.context.c = showAllStudentDetail;
+replServer.context.d = insertStudent;
---
+++ migrations/20170412043905-create-student.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Students', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      first_name: {
+        type: Sequelize.TEXT
+      },
+      last_name: {
+        type: Sequelize.TEXT
+      },
+      birth_date: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Students');
+  }
+};
