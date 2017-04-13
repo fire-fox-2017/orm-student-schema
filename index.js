fc74bc18aff4331361dac1262d9f5425346c753d
oscarhermawan <franky1790@gmail.com>
2017-04-13 15:31:47 +0700

    orm-student-done

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
 "use strict"
+
+let model = require('./models')
+
+const repl = require('repl');
+
+
+var siswa = {
+  firstName: "Oscar",
+  lastName:"Hermawan",
+  birthDate:"2006-02-10",
+  email:"haha@gmail.com",
+  height:130,
+  phone:12345677910
+}
+
+
+//CREATE
+function createSiswa(){
+  model.Student.create(siswa)
+        .then((instance)=>{
+          console.log("Data Berhasil diinput")
+        })
+        .catch( (err) => {
+          console.log("ini error: ", err.message)
+        } )
+}
+
+function showFullName(){
+  model.Student.getAllData(function(students){
+    students.forEach(function(student){
+      console.log(student.dataValues.fullName)
+    })
+  })
+}
+
+function getFullName(){
+  model.Student.findAll()
+  .then((students)=>{
+    students.forEach((student)=>{
+      console.log(student.getFullName())
+    })
+  })
+}
+
+function getAge(){
+  model.Student.findAll()
+  .then((students)=>{
+    students.forEach((student)=>{
+      console.log(`${student.getFullName()} ${student.getAge()}`)
+    })
+  })
+}
+
+
+
+let start = repl.start("> ")
+start.context.showFullName = showFullName
+start.context.getFullName = getFullName
+start.context.getAge = getAge
+start.context.createSiswa = createSiswa
